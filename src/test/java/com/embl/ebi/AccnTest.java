1f653d36c805ce724aa815d2ae275be3954ef594
sreenidhiramanuja <sreenidhi_ramanuja@yahoo.com>
2017-07-21 14:30:44 +0530

    Add files via upload

---
+++ src/test/java/com/embl/ebi/AccnTest.java
@@ -0,0 +1,45 @@
+package com.embl.ebi;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Unit test for simple App.
+ */
+public class AccnTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AccnTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AccnTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+		OrderAccnNumber testAccn = new OrderAccnNumber();
+		List<String> parseList = testAccn.Parse("ABCD000115");
+		List<String> verifyList = new ArrayList<String>();
+		verifyList.add("ABCD");
+		verifyList.add("000115");
+		assertEquals(verifyList, parseList);
+    }
+}
