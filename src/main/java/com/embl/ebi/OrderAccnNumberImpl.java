235873f77bfdb9e616a0119305d01e483e73ba17
sreenidhiramanuja <sreenidhi_ramanuja@yahoo.com>
2017-08-02 11:40:22 +0530

    Add files via upload
    Java 8 stream implementation

--- src/main/java/com/embl/ebi/OrderAccnNumberImpl.java
+++ src/main/java/com/embl/ebi/OrderAccnNumberImpl.java
@@ -5,10 +5,11 @@ import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 /**
  * 
- * This class sort the entered Accession Numbers.
+ * This class sorts the entered Accession Numbers using Java 8 Stream APIs
  * Groups the accessions numbers if they are in the range and prints them
  *
  */
@@ -25,31 +26,26 @@ public class OrderAccnNumberImpl {
 	public OrderAccnNumberImpl() {
 		input = new ArrayList<String>();
 	}
-
+	
 	// insert clean arg strings to input
-	public void addStrings(String args){
+	// Since parallel stream will not yield good performance for small collection its not utilized
+	public void addStrings(String args) {
 		// remove "," and spaces
-		String[] cleanArg = args.split(",[ ]*");
-		for (int i = 0; i < cleanArg.length; i++) {
-			if (!cleanArg[i].trim().matches("[a-zA-Z0-9]*")) {
-				continue;
-			}
-			input.add(cleanArg[i].trim());
-		}
-		Collections.sort(input);
+		Stream<String> cleanArg = Stream.of(args.split(",[ ]*"));
+		cleanArg.filter(s -> s.trim().matches("[a-zA-Z0-9]*"))
+		.forEach(s -> input.add(s.trim()));
 	}
-
-	private void printAccnNumber(Integer index) {
+	
+	private void printAccnNumber(String acnStr) {
 		if (range) {
 			range = false;
-			response = response.concat("-" + input.get(index - 1));
-			response = response.concat(", " + input.get(index));
+			response = response.concat("-" + prevAlpha + prevNum);
+			response = response.concat(", " + acnStr);
 		} else {
-			response = response.concat(", " + input.get(index));
+			response = response.concat(", " + acnStr);
 		}
 		prevAlpha = currAlpha;
 		prevNum = currNum;
-
 	}
 
 	public List<String> Parse(String str) {
@@ -60,35 +56,34 @@ public class OrderAccnNumberImpl {
 		}
 		return output;
 	}
-
-	public void printOrderedList() {
-		// print 1st element
-		response = input.get(0);
-
-		// get the alpha and numberic portion for the first sorting string
-		List<String> split = Parse(input.get(0));
-		prevAlpha = split.get(0);
-		prevNum = split.get(1);
-
-		range = false;
-		// loop from 2nd to last element in the input
-		for (int index = 1; index < input.size(); index++) {
-			split = Parse(input.get(index));
-
+	
+	void doProcessing(String acnStr) {
+		List<String> split = Parse(acnStr);
+		
+		// first entry
+		if (prevAlpha == null) {
+			// Include 1st element			
+			response = acnStr;
+			
+			// get the alpha and numeric portion for the first sorting string			
+			prevAlpha = split.get(0);
+			prevNum = split.get(1);
+			range = false;
+		} else {
 			currAlpha = split.get(0);
 			currNum = split.get(1);
 
 			// same alpha group?
 			if (prevAlpha.compareTo(currAlpha) != 0) {
-				printAccnNumber(index);
-				continue;
+				printAccnNumber(acnStr);
+				return;
 			}
 
 			// + same num length
 			// Ex: 0090 and 00091 are not in same group
 			if (prevNum.length() != currNum.length()) {
-				printAccnNumber(index);
-				continue;
+				printAccnNumber(acnStr);
+				return;
 			}
 
 			// get the last digit
@@ -98,17 +93,24 @@ public class OrderAccnNumberImpl {
 			// + not consecutive number, its consecutive digits
 			// Ex: Even 0099, 0100 are in different group
 			if ( (prevInt + 1) != currInt) {
-				printAccnNumber(index);
-				continue;
+				printAccnNumber(acnStr);
+				return;
 			}
-
+			
 			// Both are in same group; dont print curr
 			range = true;
 			prevAlpha = currAlpha;
 			prevNum = currNum;
-		}// end for
-
-	}	
+		}
+		
+	}
+	
+	public void printOrderedList() {
+		// sequential is mandated so that it can never be parallel
+		input.stream().sequential().sorted()
+		.forEach(acn -> doProcessing(acn));
+	}
+	
 
 	public String orderAccn(String args) {
 		addStrings(args);
